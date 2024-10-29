fb73cb6d803222697b5730791a9fe54c116b721e
Soiree <vanebermudez1@gmail.com>
2024-10-29 17:09:48 -0500

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2025, // required
 	month: 05, // required
-	day: 05, // required
+	day: 04, // required
 	hours: 7, // Default is 0 [0-23] integer
 	minutes: 31, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
